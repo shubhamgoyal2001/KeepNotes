76255632f9943ac0ebd9b4c2afd860f1c6b7254b
shubhamgoyal2001 <shubhamgoyal27102001@gmail.com>
2022-07-31 13:07:21 +0530

    Change Database url

--- config/mogooose.js
+++ config/mogooose.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 
-const DBURL= 'mongodb://127.0.0.1/notemake'
+// const DBURL= 'mongodb://127.0.0.1/notemake'
+const DBURL = "mongodb+srv://shubhamgoyal2710:Shubham2001@cluster0.l2f0g57.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(DBURL);
 const db=mongoose.connection;
